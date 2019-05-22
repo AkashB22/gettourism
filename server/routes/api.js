8ba5e1cb3aa43c42cde4e9425e99a09ffb3d98fa
AkashB <akashbalu22@gmail.com>
2019-05-23 02:01:33 +0530

    change from local

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://akash:Rajibalu@123@cluster0-6l2ig.mongodb.net/pgApp?retryWrites=true', function(err){
-
-mongoose.connect('mongodb://localhost/get-tourism', function(err){
-
+mongoose.connect('mongodb+srv://akash:Rajibalu%40123@cluster0-6l2ig.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true }, function(err){
     if(err) throw err;
     console.log('mongodb connected locally');
 });
-var PgDetail = require('../models/pgDetails');
+var user = require('../models/user');
 
-router.get('/pgDetails', function(req,res){
-    console.log('get request for all pgDetails');
-    const pgName = new PgDetail({
-        pgName : 'test'
+router.post('/addNewUser', function(req, res){
+    var newUser = new user({
+    firstName : req.body.firstName,
+    username : req.body.username,
+    email : req.body.email,
+    password : req.body.password,
+    country : req.body.country,
+    state : req.body.state,
+    address : req.body.address,
+    lastName: req.body.lastName,
+    zip : req.body.zip,
     });
-    PgDetail.getPgInfo(pgName, function(err, pgInfo){
+
+    user.addUser(newUser, function(err, userData){
+        console.log(userData);
         if(err) throw err;
-        res.json(pgInfo);
+        res.json(userData);
     });
 });
-
 module.exports = router;
